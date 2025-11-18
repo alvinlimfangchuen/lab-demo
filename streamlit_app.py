01167ecd1db199bacb064761b8c6c016188d7dca
Alvin <alvinlim.fangchuen@gmail.com>
2025-11-18 21:53:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 lab demo")
+st.title("🎈 lab demook")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
